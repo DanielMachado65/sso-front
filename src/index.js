2c23e2caceae6db836f4cc7b343434d3098ae113
Flavio Rochedo Foster <sife.foster@hotmail.com>
2020-06-26 12:19:45 -0300

    Edit user page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './stylesheets/app.scss';
+import './stylesheets/bootstrap.min.css';
 import './stylesheets/index.scss';
+import './stylesheets/edit.scss';
 import App from './App';
 
 ReactDOM.render(
