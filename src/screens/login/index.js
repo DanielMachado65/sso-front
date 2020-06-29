c750cc6226bb779c16098da45e98a00a9ebb1d1b
daniel machado <danielmachadopintos@gmail.com>
2020-06-29 18:29:58 -0300

    feat(login): change url

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react'
 
 import styled from 'styled-components'
+
 import { Button, Input } from 'soil'
 import SVGIcon from '../../services/SVGIcon';
 
 const Login = () => {
 
   const [visible, setVisible] = useState(false);
-  const _onForgetPassword = () => setVisible(!visible)
+  const _onVisiblePassword = () => setVisible(!visible);
+
+  const _onSubmit = e => {
+    e.preventDefault();
+    console.log('submit');
+  }
+
+  const passwordRecovered = e => {
+    e.preventDefault();
+    window.location.href = '/user/password/new'
+  }
 
   return (
     <>
@@ -34,11 +45,15 @@ const Login = () => {
               type={visible ? 'text' : 'password'}
               rightIcon={visible ? 'fa-eye' : 'fa-eye-slash'}
               style={{ marginTop: '20px', marginBottom: '5px' }}
-              onClick={_onForgetPassword} />
+              onClick={_onVisiblePassword} />
 
-            <ButtonOutlined type="link borderless forgot-pass" text="Esqueci a senha" />
+            <ButtonOutlined
+              type="link borderless forgot-pass"
+              text="Esqueci a senha"
+              onClick={passwordRecovered} />
+
             <Input type="checkbox" text="Lembrar de mim?" />
-            <ButtonSubmit type="primary" text="Entrar" href="/" target="_blank" />
+            <ButtonSubmit type="primary" text="Entrar" onClick={_onSubmit} />
           </Form>
         </Card>
       </Content>
